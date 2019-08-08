23ba89fb1dd4255d6f606513bd299f62f26a83e1
devoxx123 <mhari.devoxx@gmail.com>
2019-08-08 13:01:30 +0530

    adding extra code

--- src/main/java/com/springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/demo/SpringBootDemoApplication.java
@@ -2,11 +2,18 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication 
-public class SpringBootDemoApplication {
+@SpringBootApplication
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootDemoApplication.class);
 	}
 }
