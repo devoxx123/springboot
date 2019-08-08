518f46e66b89726164597376dfa572e1dd8c8eea
devoxx123 <mhari.devoxx@gmail.com>
2019-08-08 11:13:25 +0530

    adding new springboot application

---
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.springboot.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+
+	@RequestMapping("/findall")
+	public List<Employee> getEmployees() {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1, "Hari", "Kishore", "hari.kishore@gmail.com"));
+		employeesList.add(new Employee(2, "Anil", "Kumar", "anil.kumar@gmail.com"));
+		return employeesList;
+	}
+
+}
