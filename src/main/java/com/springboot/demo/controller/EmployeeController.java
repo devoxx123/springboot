53d28677947ad8baa3d72898a172dd5fdbf209c0
HARI KISHORE <42566418+devoxx123@users.noreply.github.com>
2019-08-28 13:42:15 +0530

    delete one employee list

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -19,7 +19,6 @@ public class EmployeeController {
 		employeesList.add(new Employee(22, "AnilKumarM", "CharanC", "anil.kumar@gmail.com"));
 		employeesList.add(new Employee(33, "SrinivasM", "Madipalli", "Srinivas@gmail.com"));
 		employeesList.add(new Employee(44, "Lepakshi", "Yanamala", "Lepakshi.jetti@gmail.com"));
-		employeesList.add(new Employee(55, "Sai", "Y", "Sai.jetti@gmail.com"));
 		return employeesList;
 	}
 
